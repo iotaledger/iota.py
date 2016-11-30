50fffa3dff40e07b103ad72ce0857e4f85e52cfd
Phoenix Zerin <phx@phx.ph>
2016-11-30 00:44:01 -0500

    Integrated bytes <-> trytes into codecs lib.

--- iota/__init__.py
+++ iota/__init__.py
@@ -5,10 +5,11 @@ from __future__ import absolute_import, division, print_function, \
 DEFAULT_PORT = 14265
 
 # Make some imports accessible from the top level of the package.
-# noinspection PyUnresolvedReferences
 from .adapter import *
-# noinspection PyUnresolvedReferences
 from .api import *
+
+# Activate TrytesCodec.
+from .codecs import *
 
 
 # Don't forget to update version number in setup.py!

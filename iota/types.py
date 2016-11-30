7abde35103c27ea4779f59f1d45c6cba7f824d56
Phoenix Zerin <phx@phx.ph>
2016-11-29 22:48:03 -0500

    Replaced Trit/Tryte impl with TryteString.

--- iota/types.py
+++ iota/types.py
@@ -2,108 +2,81 @@
 from __future__ import absolute_import, division, print_function, \
   unicode_literals
 
-from typing import Iterable, List, Optional, Text, Union
+from typing import Text, Union
+
+from six import PY2, binary_type
 
 
-class Trit(object):
+class TryteString(object):
   """
-  Ternary version of a bit.
+  A string representation of a sequence of trytes.
 
-  A trit is similar to a bit, except that it has 3 states:  1, 0 and
-    unknown.
+  A trit can be thought of as the ternary version of a bit.  It can
+    have one of three values:  1, 0 or unknown.
+
+  A tryte can be thought of as the ternary version of a byte.  It is a
+    sequence of 3 trits.
+
+  A tryte string is similar in concept to Python's byte string, except
+    it has a more limited alphabet.  Byte strings are limited to ASCII
+    (256 possible values), while the tryte string alphabet only has
+    27 characters (one for each possible tryte configuration).
   """
-  def __init__(self, value):
-    # type: (int) -> None
-    if type(value) is not int:
-      raise TypeError('Allowed types for Trit: [int].')
+  # :bc: Without the bytearray cast, Python 2 will populate the dict
+  #   with characters instead of integers.
+  # noinspection SpellCheckingInspection
+  alphabet  = dict(enumerate(bytearray(b'9ABCDEFGHIJKLMNOPQRSTUVWXYZ')))
+  index     = dict(zip(alphabet.values(), alphabet.keys()))
 
-    if not -1 <= value <= 1:
-      raise ValueError('Allowed values for Trit: [-1, 0, 1].')
+  @classmethod
+  def from_bytes(cls, bytes_):
+    # type: (Union[binary_type, bytearray]) -> TryteString
+    """Creates a TryteString from a byte string."""
+    tryte_string = bytearray()
 
-    self.value = value
+    # :bc: In Python 2, iterating over a byte string yields characters
+    #   instead of integers.
+    if not isinstance(bytes_, bytearray):
+      bytes_ = bytearray(bytes_)
+
+    for c in bytes_:
+      second, first = divmod(c, len(cls.alphabet))
+
+      tryte_string.append(cls.alphabet[first])
+      tryte_string.append(cls.alphabet[second])
+
+    return cls(tryte_string)
+
+  def __init__(self, value, pad=False):
+    # type: (Union[binary_type, bytearray], bool) -> None
+    super(TryteString, self).__init__()
+
+    if len(value) % 2:
+      if pad:
+        value += self.alphabet[0]
+      else:
+        raise ValueError(
+          'Length of TryteString must be divisible by 2.'
+        )
+
+    self.value = value if isinstance(value, bytearray) else bytearray(value)
 
   def __repr__(self):
     # type: () -> Text
-    return 'Trit({value})'.format(value=self.value)
+    return 'TryteString({value!r})'.format(value=binary_type(self.value))
 
-  def __eq__(self, other):
-    # type: (Trit) -> bool
-    return self.__cmp__(other) in (0,)
-
-  def __ne__(self, other):
-    # type: (Trit) -> bool
-    return self.__cmp__(other) in (-1, 1, None)
-
-  def __lt__(self, other):
-    # type: (Trit) -> bool
-    return self.__cmp__(other) in (-1,)
-
-  def __le__(self, other):
-    # type: (Trit) -> bool
-    return self.__cmp__(other) in (-1, 0)
-
-  def __gt__(self, other):
-    # type: (Trit) -> bool
-    return self.__cmp__(other) in (1,)
-
-  def __ge__(self, other):
-    # type: (Trit) -> bool
-    return self.__cmp__(other) in (0, 1)
-
-  def __cmp__(self, other):
-    # type: (Trit) -> Optional[int]
-    if not isinstance(other, Trit):
-      raise TypeError('Trits can only be compared to other Trits.')
-
-    if -1 in (self.value, other.value):
-      return None
-
-    # :see: https://docs.python.org/3/whatsnew/3.0.html#ordering-comparisons
-    return (self.value > other.value) - (self.value < other.value)
-
-
-class Tryte(object):
-  """
-  Ternary version of a byte.
-
-  A tryte is a sequence of three trits.
-  """
-  def __init__(self, trits, pad=False):
-    # type: (Iterable[Union[Trit, int]], bool) -> None
-    """
-    :param trits: Iterable of 3 trits (or compatible integers).
-    :param pad: Whether to pad `trits` if it has less than 3 trits.
-    """
-    super(Tryte, self).__init__()
-
-    if not isinstance(trits, Iterable):
-      raise TypeError('Allowed types for Tryte: [Iterable[Trit]].')
-
-    self.trits = [] # type: List[Trit]
-
-    for trit in trits:
-      self.trits.append(
-        trit
-          if isinstance(trit, Trit)
-          else Trit(trit)
-      )
-
-    if pad and (len(self.trits) < 3):
-      self.trits =\
-        [Trit(-1) for _ in range(0, 3 - len(self.trits))] + self.trits
-
-    if len(self.trits) != 3:
-      raise ValueError(
-        'Incorrect number of Trits for Tryte '
-        '(expected 3, actual {count}).'.format(
-          count = len(self.trits),
-        ),
-      )
-
-  def __repr__(self):
+  def __bytes__(self):
     # type: () -> Text
-    return 'Tryte({trits})'.format(trits=[trit.value for trit in self.trits])
+    """Converts the TryteString into a byte string."""
+    byte_string = bytearray()
 
-  def __getitem__(self, item):
-    # type: (int) -> Trit
-    return self.trits[item]
+    for i in range(0, len(self.value), 2):
+      byte_string.append(
+          self.index[self.value[i]]
+        + (self.index[self.value[i+1]] * len(self.index))
+      )
+
+    return binary_type(byte_string)
+
+  if PY2:
+    __str__ = __bytes__
--- test/types_test.py
+++ test/types_test.py
@@ -4,188 +4,21 @@ from __future__ import absolute_import, division, print_function, \
 
 from unittest import TestCase
 
-from iota.types import Trit, Tryte
+from six import binary_type
+
+from iota.types import TryteString
 
 
-class TritTestCase(TestCase):
-  def test_comparison(self):
-    """Testing equality and comparison of trit values."""
-    on      = Trit(1)
-    off     = Trit(0)
-    unknown = Trit(-1)
+# noinspection SpellCheckingInspection
+class TryteStringTestCase(TestCase):
+  def test_hello_world(self):
+    """PoC test for TryteString"""
+    self.assertEqual(
+      TryteString.from_bytes(b'Hello, world!').value,
+      b'RBTC9D9DCDQAEAKDCDFD9DSCFA',
+    )
 
-    self.assertTrue(on == on)
-    self.assertFalse(on != on)
-    self.assertTrue(on <= on)
-    self.assertFalse(on < on)
-    self.assertTrue(on >= on)
-    self.assertFalse(on > on)
-
-    self.assertTrue(off == off)
-    self.assertFalse(off != off)
-    self.assertTrue(off <= off)
-    self.assertFalse(off < off)
-    self.assertTrue(off >= off)
-    self.assertFalse(off > off)
-
-    # `unknown` behaves similarly to NaN.
-    # :see: https://en.wikipedia.org/wiki/IEEE_754
-    self.assertFalse(unknown == unknown)
-    self.assertTrue(unknown != unknown)
-    self.assertFalse(unknown <= unknown)
-    self.assertFalse(unknown < unknown)
-    self.assertFalse(unknown >= unknown)
-    self.assertFalse(unknown > unknown)
-
-    self.assertFalse(on == off)
-    self.assertTrue(on != off)
-    self.assertFalse(on <= off)
-    self.assertFalse(on < off)
-    self.assertTrue(on > off)
-    self.assertTrue(on >= off)
-
-    self.assertFalse(off == on)
-    self.assertTrue(off != on)
-    self.assertTrue(off <= on)
-    self.assertTrue(off < on)
-    self.assertFalse(off >= on)
-    self.assertFalse(off > on)
-
-    self.assertFalse(on == unknown)
-    self.assertTrue(on != unknown)
-    self.assertFalse(on < unknown)
-    self.assertFalse(on <= unknown)
-    self.assertFalse(on > unknown)
-    self.assertFalse(on >= unknown)
-
-    self.assertFalse(off == unknown)
-    self.assertTrue(off != unknown)
-    self.assertFalse(off < unknown)
-    self.assertFalse(off <= unknown)
-    self.assertFalse(off > unknown)
-    self.assertFalse(off >= unknown)
-
-    self.assertTrue(on is on)
-    self.assertTrue(unknown is unknown)
-
-    self.assertFalse(on is off)
-    self.assertFalse(on is Trit(1))
-    self.assertFalse(unknown is Trit(-1))
-
-    # Identity comparison also works for non-Trits.
-    self.assertFalse(on is 1)
-    self.assertFalse(unknown is None)
-
-  def test_error_invalid_value(self):
-    """Attempting to initialize a trit with an invalid value."""
-    with self.assertRaises(ValueError):
-      Trit(2)
-
-    with self.assertRaises(ValueError):
-      Trit(-2)
-
-  # noinspection PyTypeChecker
-  def test_error_invalid_type(self):
-    """Trits may only be initialized using ints."""
-    with self.assertRaises(TypeError):
-      Trit(1.0)
-
-    with self.assertRaises(TypeError):
-      # This is not allowed because it is ambiguous; did you mean
-      #   `Trit(0)` or `Trit(ord('0'))`?
-      Trit('0')
-
-    with self.assertRaises(TypeError):
-      # Technically, booleans are ints, but we still don't allow them
-      #   because Trits and booleans mean very different things.
-      Trit(False)
-
-    with self.assertRaises(TypeError):
-      # It's too easy to accidentally pass a null to the initializer.
-      #   For safety, this also is not allowed.
-      Trit(None)
-
-  # noinspection PyTypeChecker
-  def test_error_invalid_comparison(self):
-    """Trits can only be compared to other trits."""
-    with self.assertRaises(TypeError):
-      Trit(0) == 0
-
-    with self.assertRaises(TypeError):
-      Trit(1) != {}
-
-    with self.assertRaises(TypeError):
-      Trit(0) <= 0
-
-    with self.assertRaises(TypeError):
-      Trit(1) >= '0'
-
-    with self.assertRaises(TypeError):
-      Trit(-1) < 1
-
-    with self.assertRaises(TypeError):
-      Trit(-1) > False
-
-
-class TryteTestCase(TestCase):
-  def test_comparison(self):
-    """Testing equality and comparison of tryte values."""
-    # :todo: Implement test.
-    self.skipTest('Not implemented yet.')
-
-  def test_init_mixed_types(self):
-    """
-    As a convenience, you are allowed to initialize a Tryte using ints.
-    """
-    # Mixing Trits and ints is also OK.
-    tryte = Tryte([Trit(1), 1, -1])
-
-    self.assertEqual(tryte[0], Trit(1))
-    self.assertEqual(tryte[1], Trit(1))
-
-    # Well, shucks.
-    self.assertNotEqual(tryte[2], Trit(-1))
-    self.assertEqual(tryte[2].value, -1)
-
-  # noinspection PyTypeChecker
-  def test_error_init_wrong_type(self):
-    """
-    Attempting to initialize a tryte with something other than an array
-      of trits.
-    """
-    with self.assertRaises(TypeError):
-      Tryte(Trit(1))
-
-    with self.assertRaises(TypeError):
-      Tryte(['1', False, None])
-
-  def test_error_init_not_enough_trits(self):
-    """Attempting to initialize a tryte with less than 3 trits."""
-    with self.assertRaises(ValueError):
-      Tryte([])
-
-    with self.assertRaises(ValueError):
-      Tryte([Trit(0)])
-
-    with self.assertRaises(ValueError):
-      Tryte([Trit(0), Trit(0)])
-
-  def test_init_with_padding(self):
-    """Padding a tryte so that it has the correct number of trits."""
-    tryte1 = Tryte([Trit(1)], pad=True)
-
-    # Note that padding trits are applied first.
-    self.assertEqual(tryte1[0].value, -1)
-    self.assertEqual(tryte1[1].value, -1)
-    self.assertEqual(tryte1[2].value, 1)
-
-    tryte2 = Tryte([Trit(0), Trit(1)], pad=True)
-
-    self.assertEqual(tryte2[0].value, -1)
-    self.assertEqual(tryte2[1].value, 0)
-    self.assertEqual(tryte2[2].value, 1)
-
-  def test_error_init_too_many_trits(self):
-    """Attempting to initialize a tryte with more than 3 trits."""
-    with self.assertRaises(ValueError):
-      Tryte([Trit(-1), Trit(-1), Trit(-1), Trit(-1)])
+    self.assertEqual(
+      binary_type(TryteString(b'RBTC9D9DCDQAEAKDCDFD9DSCFA')),
+      b'Hello, world!',
+    )

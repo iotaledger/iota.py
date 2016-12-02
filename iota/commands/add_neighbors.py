df81a70a16e1b6faa13f35a27b2f387a64822972
Phoenix Zerin <phx@phx.ph>
2016-12-01 19:52:34 -0500

    Added validation to `addNeighbors` command.

--- iota/commands/add_neighbors.py
+++ iota/commands/add_neighbors.py
@@ -2,6 +2,9 @@
 from __future__ import absolute_import, division, print_function, \
   unicode_literals
 
+from six import string_types, text_type
+from typing import Generator, Sequence
+
 from iota.api import BaseCommand
 
 
@@ -14,7 +17,34 @@ class AddNeighborsCommand(BaseCommand):
   command = 'addNeighbors'
 
   def _prepare_request(self, request):
-    pass
+    # Required parameters.
+    uris = request['uris']
+
+    if isinstance(uris, Generator):
+      # :see: https://youtrack.jetbrains.com/issue/PY-20709
+      # noinspection PyTypeChecker
+      uris = list(uris)
+
+    if isinstance(uris, string_types) or not isinstance(uris, Sequence):
+      raise TypeError(
+        'uris has wrong type (expected Sequence, actual {type}).'.format(
+          type = type(uris).__name__,
+        ),
+      )
+
+    if not uris:
+      raise ValueError('uris must not be empty.')
+
+    for i, u in enumerate(uris):
+      if not isinstance(u, string_types):
+        raise TypeError(
+          'uris[{i}] has wrong type '
+          '(expected {expected}, actual {type}).'.format(
+            i         = i,
+            expected  = text_type.__name__,
+            type      = type(u).__name__,
+          ),
+        )
 
   def _prepare_response(self, response):
     pass

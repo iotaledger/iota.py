3a8e1ffec2f6f5b6936900c23ab10e9bdc94a629
Phoenix Zerin <phx@phx.ph>
2016-12-08 12:30:55 -0500

    Added validation to `getTrytes` request.

--- test/commands/get_trytes_test.py
+++ test/commands/get_trytes_test.py
@@ -2,9 +2,13 @@
 from __future__ import absolute_import, division, print_function, \
   unicode_literals
 
+import filters as f
 from filters.test import BaseFilterTestCase
+from six import binary_type, text_type
 
 from iota.commands.get_trytes import GetTrytesCommand
+from iota.filters import Trytes
+from iota.types import TransactionId, TryteString
 from test import MockAdapter
 
 
@@ -12,48 +16,155 @@ class GetTrytesRequestFilterTestCase(BaseFilterTestCase):
   filter_type = GetTrytesCommand(MockAdapter()).get_request_filter
   skip_value_check = True
 
+  # noinspection SpellCheckingInspection
+  def setUp(self):
+    super(GetTrytesRequestFilterTestCase, self).setUp()
+
+    # Define some valid tryte sequences that we can re-use between
+    # tests.
+    self.trytes1 = (
+      b'OAATQS9VQLSXCLDJVJJVYUGONXAXOFMJOZNSYWRZ'
+      b'SWECMXAQQURHQBJNLD9IOFEPGZEPEMPXCIVRX9999'
+    )
+
+    self.trytes2 = (
+      b'ZIJGAJ9AADLRPWNCYNNHUHRRAC9QOUDATEDQUMTN'
+      b'OTABUVRPTSTFQDGZKFYUUIE9ZEBIVCCXXXLKX9999'
+    )
+
   def test_pass_happy_path(self):
     """The request is valid."""
-    # :todo: Implement test.
-    self.skipTest('Not implemented yet.')
+    request = {
+      'hashes': [
+        TransactionId(self.trytes1),
+        TransactionId(self.trytes2),
+      ],
+    }
+
+    filter_ = self._filter(request)
+
+    self.assertFilterPasses(filter_)
+    self.assertDictEqual(filter_.cleaned_data, request)
 
   def test_pass_compatible_types(self):
     """
     The request contains values that can be converted to the expected
     types.
     """
-    # :todo: Implement test.
-    self.skipTest('Not implemented yet.')
+    filter_ = self._filter({
+      'hashes': [
+        # Any sequence that can be converted into a TransactionId is
+        # valid.
+        binary_type(self.trytes1),
+        bytearray(self.trytes2),
+      ],
+    })
+
+    self.assertFilterPasses(filter_)
+    self.assertDictEqual(
+      filter_.cleaned_data,
+
+      {
+        'hashes': [
+          TransactionId(self.trytes1),
+          TransactionId(self.trytes2),
+        ],
+      },
+    )
 
   def test_fail_empty(self):
     """The request is empty."""
-    # :todo: Implement test.
-    self.skipTest('Not implemented yet.')
+    self.assertFilterErrors(
+      {},
+
+      {
+        'hashes': [f.FilterMapper.CODE_MISSING_KEY],
+      },
+    )
 
   def test_fail_unexpected_parameters(self):
     """The request contains unexpected parameters."""
-    # :todo: Implement test.
-    self.skipTest('Not implemented yet.')
+    self.assertFilterErrors(
+      {
+        'hashes': [TransactionId(self.trytes1)],
+
+        # This is why we can't have nice things!
+        'foo': 'bar',
+      },
+
+      {
+        'foo': [f.FilterMapper.CODE_EXTRA_KEY],
+      },
+    )
 
   def test_fail_hashes_null(self):
     """`hashes` is null."""
-    # :todo: Implement test.
-    self.skipTest('Not implemented yet.')
+    self.assertFilterErrors(
+      {
+        'hashes': None,
+      },
+
+      {
+        'hashes': [f.Required.CODE_EMPTY],
+      },
+    )
 
   def test_fail_hashes_wrong_type(self):
     """`hashes` is not an array."""
-    # :todo: Implement test.
-    self.skipTest('Not implemented yet.')
+    self.assertFilterErrors(
+      {
+        # `hashes` must be an array, even if we're only querying
+        # against a single transaction.
+        'hashes': TransactionId(self.trytes1),
+      },
+
+      {
+        'hashes': [f.Type.CODE_WRONG_TYPE],
+      },
+    )
 
   def test_fail_hashes_empty(self):
     """`hashes` is an array, but it is empty."""
-    # :todo: Implement test.
-    self.skipTest('Not implemented yet.')
+    self.assertFilterErrors(
+      {
+        'hashes': [],
+      },
+
+      {
+        'hashes': [f.Required.CODE_EMPTY],
+      },
+    )
 
   def test_fail_hashes_contents_invalid(self):
     """`hashes` is an array, but it contains invalid values."""
-    # :todo: Implement test.
-    self.skipTest('Not implemented yet.')
+    self.assertFilterErrors(
+      {
+        'hashes': [
+          b'',
+          text_type(self.trytes1, 'ascii'),
+          True,
+          None,
+          b'not valid trytes',
+
+          # This is actually valid; I just added it to make sure the
+          #   filter isn't cheating!
+          TryteString(self.trytes1),
+
+          2130706433,
+          b'9' * 82,
+        ],
+      },
+
+      {
+        'hashes.0': [f.Required.CODE_EMPTY],
+        'hashes.1': [f.Type.CODE_WRONG_TYPE],
+        'hashes.2': [f.Type.CODE_WRONG_TYPE],
+        'hashes.3': [f.Required.CODE_EMPTY],
+        'hashes.4': [Trytes.CODE_NOT_TRYTES],
+        'hashes.6': [f.Type.CODE_WRONG_TYPE],
+        'hashes.7': [Trytes.CODE_WRONG_FORMAT],
+      },
+    )
 
 
 class GetTrytesResponseFilter(BaseFilterTestCase):

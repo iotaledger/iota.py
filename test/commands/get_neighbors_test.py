439f16a0f5233a1bcb863bbc04fd0fda842bd22d
Phoenix Zerin <phx@phx.ph>
2016-12-08 09:41:43 -0500

    Added validation to `getNeighbors` requests.

---
+++ test/commands/get_neighbors_test.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, \
+  unicode_literals
+
+import filters as f
+from filters.test import BaseFilterTestCase
+
+from iota.commands.get_neighbors import GetNeighborsCommand
+from test import MockAdapter
+
+
+class GetNeighborsRequestFilterTestCase(BaseFilterTestCase):
+  filter_type = GetNeighborsCommand(MockAdapter()).get_request_filter
+  skip_value_check = True
+
+  def test_pass_empty(self):
+    """The request is (correctly) empty."""
+    filter_ = self._filter({})
+
+    self.assertFilterPasses(filter_)
+    self.assertDictEqual(filter_.cleaned_data, {})
+
+  def test_fail_unexpected_parameters(self):
+    """The request contains unexpected parameters."""
+    self.assertFilterErrors(
+      {
+        # Fool of a Took!
+        'foo': 'bar',
+      },
+
+      {
+        'foo': [f.FilterMapper.CODE_EXTRA_KEY],
+      },
+    )

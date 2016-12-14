0caaa100ebc0e1ec010327b96a32ae4b99aeaaf7
Phoenix Zerin <phx@phx.ph>
2016-12-13 21:27:26 -0500

    Implemented `sendTrytes`, made code more DRY.

--- iota/commands/broadcast_and_store.py
+++ iota/commands/broadcast_and_store.py
@@ -2,11 +2,9 @@
 from __future__ import absolute_import, division, print_function, \
   unicode_literals
 
-import filters as f
-from iota.commands import FilterCommand, RequestFilter
+from iota.commands import FilterCommand
 from iota.commands.broadcast_transactions import BroadcastTransactionsCommand
 from iota.commands.store_transactions import StoreTransactionsCommand
-from iota.filters import Trytes
 
 __all__ = [
   'BroadcastAndStoreCommand',
@@ -22,18 +20,20 @@ class BroadcastAndStoreCommand(FilterCommand):
   command = 'broadcastAndStore'
 
   def get_request_filter(self):
-    return BroadcastAndStoreRequestFilter()
+    pass
 
   def get_response_filter(self):
     pass
 
-  def send_request(self, request):
-    BroadcastTransactionsCommand(self.adapter).send_request(request)
-    return StoreTransactionsCommand(self.adapter).send_request(request)
+  def _send_request(self, request):
+    bt_command = BroadcastTransactionsCommand(
+      adapter         = self.adapter,
+      prepare_request = self.prepare_request,
+    )
+    bt_command(**request)
 
-
-class BroadcastAndStoreRequestFilter(RequestFilter):
-  def __init__(self):
-    super(BroadcastAndStoreRequestFilter, self).__init__({
-      'trytes': f.Required | f.Array | f.FilterRepeater(f.Required | Trytes),
-    })
+    # `storeTransactions` accepts the exact same request object as
+    # `broadcastTransactions`, so it's safe to bypass request
+    # validation here.
+    return \
+      StoreTransactionsCommand(self.adapter, prepare_request=False)(**request)

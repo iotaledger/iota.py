a8796688752b8da8b1861ebbfdc8197b599c5504
Phoenix Zerin <phx@phx.ph>
2016-12-21 12:57:44 -0500

    Removed custom Seed type.
    Seeds are not supposed to be padded automatically; this will be fixed in a future commit.

--- iota/crypto/signing.py
+++ iota/crypto/signing.py
@@ -6,113 +6,11 @@ from typing import Generator, List, MutableSequence
 
 from iota import TryteString, TrytesCompatible
 from iota.crypto import Curl, HASH_LENGTH
+from iota.crypto.types import SigningKey
 
 __all__ = [
   'KeyGenerator',
-  'Seed',
-  'SigningKey',
 ]
-
-
-class Seed(TryteString):
-  """
-  A TryteString that acts as a seed for generating new keys.
-  """
-  LEN = 81
-
-  def __init__(self, trytes):
-    # type: (TrytesCompatible) -> None
-    super(Seed, self).__init__(trytes, pad=self.LEN)
-
-    if len(self._trytes) > self.LEN:
-      raise ValueError('{cls} values must be {len} trytes long.'.format(
-        cls = type(self).__name__,
-        len = self.LEN
-      ))
-
-
-class SigningKey(TryteString):
-  """
-  A TryteString that acts as a signing key, e.g., for generating
-  message signatures, new addresses, etc.
-  """
-  BLOCK_LEN = 2187
-  """
-  Similar to RSA keys, SigningKeys must have a length that is divisible
-  by a certain number of trytes.
-  """
-
-  def __init__(self, trytes):
-    # type: (TrytesCompatible) -> None
-    super(SigningKey, self).__init__(trytes)
-
-    if len(self._trytes) % self.BLOCK_LEN:
-      raise ValueError(
-        'Length of {cls} values must be a multiple of {len} trytes.'.format(
-          cls = type(self).__name__,
-          len = self.BLOCK_LEN
-        ),
-      )
-
-  @property
-  def block_count(self):
-    # type: () -> int
-    """
-    Returns the length of this key, expressed in blocks.
-    """
-    return len(self) // self.BLOCK_LEN
-
-  def get_digest_trits(self):
-    # type: () -> List[int]
-    """
-    Generates the digest used to do the actual signing.
-
-    Signing keys can have variable length and tend to be quite long,
-    which makes them not-well-suited for use in crypto algorithms.
-
-    The digest is essentially the result of running the signing key
-    through a PBKDF, yielding a constant-length hash that can be used
-    for crypto.
-    """
-    # Multiply by 3 to convert trytes into trits.
-    block_size  = self.BLOCK_LEN * 3
-    raw_trits   = self.as_trits()
-
-    # Initialize list with the correct length to improve performance.
-    digest = [0] * HASH_LENGTH  # type: List[int]
-
-    for i in range(self.block_count):
-      block_start = i * block_size
-      block_end   = block_start + block_size
-
-      block_trits = raw_trits[block_start:block_end]
-
-      # Initialize ``key_fragment`` with the correct length to
-      # improve performance.
-      key_fragment = [0] * block_size # type: List[int]
-
-      buffer = [] # type: List[int]
-
-      for j in range(27):
-        hash_start = j * HASH_LENGTH
-        hash_end   = hash_start + HASH_LENGTH
-
-        buffer = block_trits[hash_start:hash_end]
-
-        for k in range(26):
-          sponge = Curl()
-          sponge.absorb(buffer)
-          sponge.squeeze(buffer)
-
-        key_fragment[hash_start:hash_end] = buffer
-
-      sponge = Curl()
-      sponge.absorb(key_fragment)
-      sponge.squeeze(buffer)
-
-      digest[block_start:block_end] = buffer
-
-    return digest
 
 
 class KeyGenerator(object):
@@ -123,7 +21,7 @@ class KeyGenerator(object):
     # type: (TrytesCompatible) -> None
     super(KeyGenerator, self).__init__()
 
-    self.seed = Seed(seed).as_trits()
+    self.seed = TryteString(seed, pad=81).as_trits()
 
   def get_keys(self, start, count=1, step=1, iterations=1):
     # type: (int, int, int, int) -> List[SigningKey]
@@ -225,7 +123,7 @@ class KeyGenerator(object):
         for j in range(27):
           # Multiply by 3 because sponge works with trits, but
           # ``Seed.LEN`` is a quantity of trytes.
-          buffer = [0] * (Seed.LEN * 3) # type: MutableSequence[int]
+          buffer = [0] * HASH_LENGTH # type: MutableSequence[int]
           sponge.squeeze(buffer)
           key += buffer
 

e57d75337e28d99bf111b58024594c901cb991c7
Phoenix Zerin <phx@phx.ph>
2016-12-22 17:29:54 -0500

    Stubbed out unit tests for `prepareTransfer`.

--- test/commands/extended/prepare_transfers_test.py
+++ test/commands/extended/prepare_transfers_test.py
@@ -14,6 +14,119 @@ class PrepareTransfersRequestFilterTestCase(BaseFilterTestCase):
   filter_type = PrepareTransfersCommand(MockAdapter()).get_request_filter
   skip_value_check = True
 
+  def test_pass_happy_path(self):
+    """
+    Request is valid.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_pass_compatible_types(self):
+    """
+    Request contains values that can be converted to the expected
+    types.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_pass_optional_parameters_omitted(self):
+    """
+    Request omits optional parameters.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_fail_empty(self):
+    """
+    Request is empty.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_fail_unexpected_parameters(self):
+    """
+    Request contains unexpected parameters.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_fail_seed_null(self):
+    """
+    ``seed`` is null.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_fail_seed_wrong_type(self):
+    """
+    ``seed`` is not a TrytesCompatible value.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_fail_seed_not_trytes(self):
+    """
+    ``seed`` contains invalid characters.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_fail_transfers_wrong_type(self):
+    """
+    ``transfers`` is not an array.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_fail_transfers_empty(self):
+    """
+    ``transfers`` is an array, but it is empty.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_fail_transfers_contents_invalid(self):
+    """
+    ``transfers`` is a non-empty array, but it contains invalid values.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_fail_change_address_wrong_type(self):
+    """
+    ``change_address`` is not a TrytesCompatible value.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_fail_change_address_not_trytes(self):
+    """
+    ``change_address`` contains invalid characters.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_fail_inputs_wrong_type(self):
+    """
+    ``inputs`` is not an array.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_fail_inputs_empty(self):
+    """
+    ``inputs`` is an array, but it is empty.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+  def test_fail_inputs_contents_invalid(self):
+    """
+    ``inputs`` is a non-empty array, but it contains invalid values.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
 
 class PrepareTransfersCommandTestCase(TestCase):
   def setUp(self):
@@ -29,3 +142,5 @@ class PrepareTransfersCommandTestCase(TestCase):
       Iota(self.adapter).prepareTransfers,
       PrepareTransfersCommand,
     )
+
+  # :todo: Unit tests.

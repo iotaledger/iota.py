09c8c37ddb645d3c9c6dd290cf247cd73ff5adc8
Phoenix Zerin <phx@phx.ph>
2017-05-01 10:30:40 -0500

    [#10] Stubbed out unit tests for `prepare_multisig_transfer`.

--- src/iota/multisig/commands/__init__.py
+++ src/iota/multisig/commands/__init__.py
@@ -6,3 +6,4 @@ from __future__ import absolute_import, division, print_function, \
 from .create_multisig_address import *
 from .get_digests import *
 from .get_private_keys import *
+from .prepare_multisig_transfer import *

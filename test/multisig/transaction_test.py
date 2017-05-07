1908b544d074a053439cad027578ab78e0e7cab6
Phoenix Zerin <phx@phx.ph>
2017-05-07 09:02:39 -0500

    [#10] Multisig inputs must have security level.

--- test/multisig/transaction_test.py
+++ test/multisig/transaction_test.py
@@ -5,30 +5,84 @@ from __future__ import absolute_import, division, print_function, \
 from unittest import TestCase
 
 from iota import Address, ProposedTransaction
+from iota.crypto.types import Digest
 from iota.multisig.transaction import ProposedMultisigBundle
 from iota.multisig.types import MultisigAddress
 
 
 class ProposedMultisigBundleTestCase(TestCase):
+  # noinspection SpellCheckingInspection
   def setUp(self):
     super(ProposedMultisigBundleTestCase, self).setUp()
 
     self.bundle = ProposedMultisigBundle()
 
+    # Define some tryte sequences that we can reuse between tests.
+    self.digest_1 =\
+      Digest(
+        trytes =
+          b'FWNEPVJNGUKTSHSBDO9AORBCVWWLVXC9KAMKYYNKPYNJDKSAUURI9ELKOEEYPKVTYP'
+          b'CKOCJQESYFEMINIFKX9PDDGRBEEHYYXCJW9LHGWFZGHKCPVDBGMGQKIPCNKNITGMZT'
+          b'DIWVUB9PCHCOPHMIWKSUKRHZOJPMAY',
+
+        key_index = 0,
+      )
+
+    self.digest_2 =\
+      Digest(
+        trytes =
+          b'PAIRLDJQY9XAUSKIGCTHRJHZVARBEY9NNHYJ9UI9HWWZXFSDWEZEGDCWNVVYSYDV9O'
+          b'HTR9NGGZURISWTNECFTCMEWQQFJ9VKLFPDTYJYXC99OLGRH9OSFJLMEOGHFDHZYEAF'
+          b'IMIZTJRBQUVCR9U9ZWTMUXTUEOUBLC',
+
+        key_index = 0,
+      )
+
+    self.trytes_1 = (
+      b'TESTVALUE9DONTUSEINPRODUCTION99999IIPEM9'
+      b'LA9FLHEGHDACSA9DOBQHQCX9BBHCFDIIMACARHA9B'
+    )
+
+    self.trytes_2 = (
+      b'TESTVALUE9DONTUSEINPRODUCTION99999BGUDVE'
+      b'DGH9WFQDEDVETCOGEGCDI9RFHGFGXBI99EJICHNEM'
+    )
+
+    self.trytes_3 = (
+      b'TESTVALUE9DONTUSEINPRODUCTION99999XBGEUC'
+      b'LF9EIFXHM9KHQANBLBHFVGTEGBWHNAKFDGZHYGCHI'
+    )
+
+  def test_add_inputs_happy_path(self):
+    """
+    Adding a multisig input to a bundle.
+    """
+    # :todo: Implement test.
+    self.skipTest('Not implemented yet.')
+
+    # noinspection SpellCheckingInspection
+    self.bundle.add_transaction(
+      ProposedTransaction(
+        address = Address(self.trytes_1),
+        value   = 42,
+      ),
+    )
+
+    self.bundle.add_inputs([
+      MultisigAddress(
+        trytes  = self.trytes_2,
+        digests = [self.digest_1, self.digest_2],
+      )
+    ])
+
   def test_add_inputs_error_already_finalized(self):
     """
     Attempting to add inputs to a bundle that is already finalized.
     """
-    # noinspection SpellCheckingInspection
     self.bundle.add_transaction(
       ProposedTransaction(
-        address =
-          Address(
-            b'TESTVALUE9DONTUSEINPRODUCTION99999XE9IVG'
-            b'EFNDOCQCMERGUATCIEGGOHPHGFIAQEZGNHQ9W99CH'
-          ),
-
-        value = 0,
+        address = Address(self.trytes_1),
+        value   = 0,
       ),
     )
 
@@ -36,6 +90,18 @@ class ProposedMultisigBundleTestCase(TestCase):
 
     with self.assertRaises(RuntimeError):
       self.bundle.add_inputs([])
+
+  def test_add_inputs_error_digests_empty(self):
+    """
+    Adding a multisig input with unknown digests.
+    """
+    with self.assertRaises(ValueError):
+      self.bundle.add_inputs([
+        MultisigAddress(
+          trytes  = self.trytes_1,
+          digests = [],
+        ),
+      ])
 
   def test_add_inputs_error_multiple(self):
     """
@@ -47,19 +113,13 @@ class ProposedMultisigBundleTestCase(TestCase):
       # noinspection SpellCheckingInspection
       self.bundle.add_inputs([
         MultisigAddress(
-          trytes =
-            b'TESTVALUE9DONTUSEINPRODUCTION99999XDIFTE'
-            b'RCA9CHWGBEJHZGXENCDHUCADDEMDP9PAG9BBA9AAC',
-
-          digests = [],
+          trytes  = self.trytes_1,
+          digests = [self.digest_1, self.digest_2],
         ),
 
         MultisigAddress(
-          trytes =
-            b'TESTVALUE9DONTUSEINPRODUCTION99999Y9EHHI'
-            b'9GD9OCRGDDVFGEQCN9QFNATAKARCOBV99BJDGEADT',
-
-          digests = [],
+          trytes  = self.trytes_2,
+          digests = [self.digest_2, self.digest_1],
         ),
       ])
 
@@ -73,11 +133,8 @@ class ProposedMultisigBundleTestCase(TestCase):
       # noinspection SpellCheckingInspection,PyTypeChecker
       self.bundle.add_inputs([
         Address(
-          trytes =
-            b'TESTVALUE9DONTUSEINPRODUCTION99999QGSEW9'
-            b'DADCMCFICIZCTDUCKI9CJAAHNCEHBIHBPFQB9G9FS',
-
+          trytes    = self.trytes_1,
           key_index = 0,
           balance   = 76,
-        )
+        ),
       ])

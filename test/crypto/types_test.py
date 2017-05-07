686783796c1c16b27eef170157e86c722de8d613
Phoenix Zerin <phx@phx.ph>
2017-05-07 09:49:24 -0500

    Digest length must be a multiple of 81 trytes.

--- test/crypto/types_test.py
+++ test/crypto/types_test.py
@@ -4,8 +4,30 @@ from __future__ import absolute_import, division, print_function, \
 
 from unittest import TestCase
 
-from iota import TryteString
-from iota.crypto.types import PrivateKey
+from iota import Hash, TryteString
+from iota.crypto.types import Digest, PrivateKey
+
+
+class DigestTestCase(TestCase):
+  def test_init_error_too_long(self):
+    """
+    A digest's length must be a multiple of :py:attr:`Hash.LEN` trytes.
+    """
+    with self.assertRaises(ValueError):
+      Digest(b'9' * (Hash.LEN + 1))
+
+    with self.assertRaises(ValueError):
+      Digest(b'9' * (2 * Hash.LEN + 1))
+
+  def test_init_error_too_short(self):
+    """
+    A digest's length must be a multiple of :py:attr:`Hash.LEN` trytes.
+    """
+    with self.assertRaises(ValueError):
+      Digest(b'9' * (Hash.LEN - 1))
+
+    with self.assertRaises(ValueError):
+      Digest(b'9' * (2 * Hash.LEN - 1))
 
 
 # noinspection SpellCheckingInspection

a319a2daf9a69d77aec0eebf671c490f60cdfac8
Phoenix Zerin <phx@phx.ph>
2017-07-09 16:49:41 +1200

    Bumping version number.

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
   name        = 'PyOTA',
   description = 'IOTA API library for Python',
   url         = 'https://github.com/iotaledger/iota.lib.py',
-  version     = '1.1.3',
+  version     = '1.2.0b1',
 
   long_description = long_description,
 

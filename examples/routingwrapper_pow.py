51ec2166a6823fde6f29ce9028feb43b72b5d2de
Phoenix Zerin <phx@phx.ph>
2017-07-30 08:00:36 +1200

    Fixed indent.

--- examples/routingwrapper_pow.py
+++ examples/routingwrapper_pow.py
@@ -20,24 +20,24 @@ api =\
 # Example of sending a transfer using the adapter.
 bundle = api.send_transfer(
   depth = 100,
-    transfers = [
-      ProposedTransaction(
-        # Recipient of the transfer.
-        address =
-          Address(
-            #b'TESTVALUE9DONTUSEINPRODUCTION99999FBFFTG'
-            #b'QFWEHEL9KCAFXBJBXGE9HID9XCOHFIDABHDG9AHDR'
-          ),
+  transfers = [
+    ProposedTransaction(
+      # Recipient of the transfer.
+      address =
+        Address(
+          #b'TESTVALUE9DONTUSEINPRODUCTION99999FBFFTG'
+          #b'QFWEHEL9KCAFXBJBXGE9HID9XCOHFIDABHDG9AHDR'
+        ),
 
-        # Amount of IOTA to transfer.
-        # This value may be zero.
-        value = 1,
+      # Amount of IOTA to transfer.
+      # This value may be zero.
+      value = 1,
 
-        # Optional tag to attach to the transfer.
-        tag = Tag(b'ADAPT'),
+      # Optional tag to attach to the transfer.
+      tag = Tag(b'ADAPT'),
 
-        # Optional message to include with the transfer.
-        message = TryteString.from_string('Hello!'),
-      ),
-    ],
+      # Optional message to include with the transfer.
+      message = TryteString.from_string('Hello!'),
+    ),
+  ],
 )

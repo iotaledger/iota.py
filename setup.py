f097a905796c75368e19faefefadb4d9723a76ad
Phoenix Zerin <phx@phx.ph>
2017-09-24 19:19:06 +1300

    Added missing support files to source distribution.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
   name        = 'PyOTA',
   description = 'IOTA API library for Python',
   url         = 'https://github.com/iotaledger/iota.lib.py',
-  version     = '2.0.0b1',
+  version     = '2.0.0b2',
 
   long_description = long_description,
 

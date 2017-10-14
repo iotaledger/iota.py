393d62dccaeac965b31b1349a0ae197f616bd999
Phoenix Zerin <phx@phx.ph>
2017-10-15 11:53:14 +1300

    [#49] `trits_from_int` now defaults to `pad=1`.

--- test/types_test.py
+++ test/types_test.py
@@ -6,8 +6,31 @@ from unittest import TestCase
 
 from six import binary_type, text_type
 
-from iota import Address, AddressChecksum, Hash, Tag, TryteString, TrytesCodec, \
-  TrytesDecodeError
+from iota import Address, AddressChecksum, Hash, Tag, TryteString, \
+  TrytesCodec, TrytesDecodeError, trits_from_int
+
+
+class TritsFromIntTestCase(TestCase):
+  """
+  Explicit unit tests for :py:func:`trits_from_int`.
+
+  Note that this function is used internally by many different classes
+  and functions, so we still have good coverage even though this
+  particular test case has limited scope.
+  """
+  def test_zero(self):
+    """
+    Zero is represented as ``[0]`` by default.
+
+    https://github.com/iotaledger/iota.lib.py/issues/49
+    """
+    self.assertEqual(trits_from_int(0), [0])
+
+  def test_zero_unpadded(self):
+    """
+    Converting zero to trits, without padding.
+    """
+    self.assertEqual(trits_from_int(0, pad=None), [])
 
 
 # noinspection SpellCheckingInspection

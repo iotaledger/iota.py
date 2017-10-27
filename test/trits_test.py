643d55811857884f56d4f1c2fc1d1bb20b38deeb
Phoenix Zerin <phx@phx.ph>
2017-10-28 11:37:15 +1300

    [#84] Made trit addition match JS lib.

---
+++ test/trits_test.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, \
+  unicode_literals
+
+from unittest import TestCase
+
+from iota import trits_from_int
+
+
+class TritsFromIntTestCase(TestCase):
+  """
+  Explicit unit tests for :py:func:`trits_from_int`.
+
+  Note that this function is used internally by many different classes
+  and functions, so we still have good coverage even though this
+  particular test case has limited scope.
+  """
+  def test_zero(self):
+    """
+    Zero is represented as ``[0]`` by default.
+
+    https://github.com/iotaledger/iota.lib.py/issues/49
+    """
+    self.assertEqual(trits_from_int(0), [0])
+
+  def test_zero_unpadded(self):
+    """
+    Converting zero to trits, without padding.
+    """
+    self.assertEqual(trits_from_int(0, pad=None), [])
+
+

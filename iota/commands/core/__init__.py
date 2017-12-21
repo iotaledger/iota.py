0edbb69f2317f02498b4b7a49658451c8630b2f7
scottbelden <scottabelden@gmail.com>
2017-12-21 16:16:43 -0500

    add check_consistency api

--- iota/commands/core/__init__.py
+++ iota/commands/core/__init__.py
@@ -13,6 +13,7 @@ from __future__ import absolute_import, division, print_function, \
 from .add_neighbors import *
 from .attach_to_tangle import *
 from .broadcast_transactions import *
+from .check_consistency import *
 from .find_transactions import *
 from .get_balances import *
 from .get_inclusion_states import *

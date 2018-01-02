d15e2be63dc7141a6a1e0f9f2dff5a83a79c04d2
scottbelden <scottabelden@gmail.com>
2018-01-02 10:53:06 -0500

    add helpers namespace

--- iota/commands/extended/__init__.py
+++ iota/commands/extended/__init__.py
@@ -18,7 +18,6 @@ from .get_inputs import *
 from .get_latest_inclusion import *
 from .get_new_addresses import *
 from .get_transfers import *
-from .is_promotable import *
 from .prepare_transfer import *
 from .replay_bundle import *
 from .send_transfer import *

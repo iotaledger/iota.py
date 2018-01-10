1043297e435e420116fc15f6adf3ce590ecd517f
Louie Lu <git@louie.lu>
2018-01-10 11:25:00 +0800

    Change curl import

--- test/crypto/pycurl_test.py
+++ test/crypto/pycurl_test.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, division, print_function, \
 from unittest import TestCase
 
 from iota import TryteString
-from iota.crypto.pycurl import Curl
+from iota.crypto import Curl
 
 
 class TestCurl(TestCase):

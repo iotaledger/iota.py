3b4a0cf0a47da91bac8ea1213ac18a6c634af065
Johannes Innerbichler <j.innerbichler@gmail.com>
2018-01-14 00:53:32 +0100

    added test for single address

--- test/commands/extended/is_reattachable_test.py
+++ test/commands/extended/is_reattachable_test.py
@@ -59,24 +59,6 @@ class IsReattachableRequestFilterTestCase(BaseFilterTestCase):
       },
     )
 
-  # def test_pass_single_address(self):
-  #   """
-  #   The incoming request contains a single address
-  #   """
-  #   request = {
-  #     'addresses': Address(self.address_1)
-  #   }
-  #
-  #   filter_ = self._filter(request)
-  #
-  #   self.assertFilterPasses(filter_)
-  #   self.assertDictEqual(
-  #     filter_.cleaned_data,
-  #     {
-  #       'addresses': text_type(Address(self.address_1))
-  #     },
-  #   )
-
   def test_pass_compatible_types(self):
     """
     The incoming request contains values that can be converted to the
@@ -128,6 +110,21 @@ class IsReattachableRequestFilterTestCase(BaseFilterTestCase):
       {
         'addresses': [f.FilterMapper.CODE_MISSING_KEY],
       },
+    )
+
+  def test_fail_single_address(self):
+    """
+    The incoming request contains a single address
+    """
+    request = {
+      'addresses': Address(self.address_1)
+    }
+
+    self.assertFilterErrors(
+      request,
+      {
+        'addresses': [f.Type.CODE_WRONG_TYPE],
+      }
     )
 
 

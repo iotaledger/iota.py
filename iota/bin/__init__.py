89d3511eaade9f3d647253977e9775fca4ed67f2
miili <marius.isken@gmail.com>
2018-01-25 23:54:49 +0100

    bin: pep8

--- iota/bin/__init__.py
+++ iota/bin/__init__.py
@@ -1,14 +1,13 @@
 # coding=utf-8
 from __future__ import absolute_import, division, print_function, \
-  unicode_literals
+    unicode_literals
 
 import sys
 from abc import ABCMeta, abstractmethod as abstract_method
 from argparse import ArgumentParser
 from getpass import getpass as secure_input
-from io import StringIO
 from sys import exit
-from typing import Optional, Text
+from typing import Optional, Text  # noqa
 
 from six import text_type, with_metaclass
 
@@ -16,162 +15,152 @@ from iota import Iota, __version__
 from iota.crypto.types import Seed
 
 __all__ = [
-  'IotaCommandLineApp',
+    'IotaCommandLineApp',
 ]
 
 
 class IotaCommandLineApp(with_metaclass(ABCMeta)):
-  """
-  Base functionality for a PyOTA-powered command-line application.
-  """
-  requires_seed = True
-  """
-  Whether the command requires the user to provide a seed.
-  """
-
-  def __init__(self, stdout=sys.stdout, stderr=sys.stderr, stdin=sys.stdin):
-    # type: (StringIO, StringIO, StringIO) -> None
-    super(IotaCommandLineApp, self).__init__()
-
-    self.stdout = stdout
-    self.stderr = stderr
-    self.stdin  = stdin
-
-  @abstract_method
-  def execute(self, api, **arguments):
-    # type: (Iota, ...) -> Optional[int]
     """
-    Executes the command and (optionally) returns an exit code (used by
-    the shell to determine if the application exited cleanly).
-
-    :param api:
-      The API object used to communicate with the node.
-
-    :param arguments:
-      Command-line arguments parsed by the argument parser.
+    Base functionality for a PyOTA-powered command-line application.
     """
-    raise NotImplementedError(
-      'Not implemented in {cls}.'.format(cls=type(self).__name__),
-    )
-
-  def main(self):
+    requires_seed = True
     """
-    Executes the command from :py:data:`sys.argv` and exits.
+    Whether the command requires the user to provide a seed.
     """
-    exit(self.run_from_argv())
 
-  def run_from_argv(self, argv=None):
-    # type: (Optional[tuple]) -> int
-    """
-    Executes the command from a collection of arguments (e.g.,
-    :py:data`sys.argv`) and returns the exit code.
+    def __init__(self, stdout=sys.stdout, stderr=sys.stderr, stdin=sys.stdin):
+        # type: (StringIO, StringIO, StringIO) -> None
+        super(IotaCommandLineApp, self).__init__()
 
-    :param argv:
-      Arguments to pass to the argument parser.
-      If ``None``, defaults to ``sys.argv[1:]``.
-    """
-    exit_code = self.execute(**self.parse_argv(argv))
+        self.stdout = stdout
+        self.stderr = stderr
+        self.stdin = stdin
 
-    if exit_code is None:
-      exit_code = 0
+    @abstract_method
+    def execute(self, api, **arguments):
+        # type: (Iota, ...) -> Optional[int]
+        """
+        Executes the command and (optionally) returns an exit code (used by
+        the shell to determine if the application exited cleanly).
 
-    return exit_code
+        :param api:
+            The API object used to communicate with the node.
 
-  def parse_argv(self, argv=None):
-    # type: (Optional[tuple]) -> dict
-    """
-    Parses arguments for the command.
+        :param arguments:
+            Command-line arguments parsed by the argument parser.
+        """
+        raise NotImplementedError(
+            'Not implemented in {cls}.'.format(cls=type(self).__name__)
+        )
 
-    :param argv:
-      Arguments to pass to the argument parser.
-      If ``None``, defaults to ``sys.argv[1:]``.
-    """
-    arguments = vars(self.create_argument_parser().parse_args(argv))
+    def main(self):
+        """
+        Executes the command from :py:data:`sys.argv` and exits.
+        """
+        exit(self.run_from_argv())
 
-    seed = None
-    if self.requires_seed:
-      seed_filepath = arguments.pop('seed_file')
+    def run_from_argv(self, argv=None):
+        # type: (Optional[tuple]) -> int
+        """
+        Executes the command from a collection of arguments (e.g.,
+        :py:data`sys.argv`) and returns the exit code.
 
-      seed = (
-        self.seed_from_filepath(seed_filepath)
-          if seed_filepath
-          else self.prompt_for_seed()
-      )
+        :param argv:
+            Arguments to pass to the argument parser.
+            If ``None``, defaults to ``sys.argv[1:]``.
+        """
+        exit_code = self.execute(**self.parse_argv(argv))
 
-    arguments['api'] =\
-      Iota(
-        adapter = arguments.pop('uri'),
-        seed    = seed,
-        testnet = arguments.pop('testnet'),
-      )
+        if exit_code is None:
+            exit_code = 0
 
-    return arguments
+        return exit_code
 
-  def create_argument_parser(self):
-    # type: () -> ArgumentParser
-    """
-    Returns the argument parser that will be used to interpret
-    arguments and options from argv.
-    """
-    parser = ArgumentParser(
-      description = self.__doc__,
-      epilog      = 'PyOTA v{version}'.format(version=__version__),
-    )
+    def parse_argv(self, argv=None):
+        # type: (Optional[tuple]) -> dict
+        """
+        Parses arguments for the command.
 
-    parser.add_argument(
-      '--uri',
-        type    = text_type,
-        default = 'http://localhost:14265/',
+        :param argv:
+            Arguments to pass to the argument parser.
+            If ``None``, defaults to ``sys.argv[1:]``.
+        """
+        arguments = vars(self.create_argument_parser().parse_args(argv))
 
-        help =
-          'URI of the node to connect to '
-          '(defaults to http://localhost:14265/).',
-    )
+        seed = None
+        if self.requires_seed:
+            seed_filepath = arguments.pop('seed_file')
 
-    if self.requires_seed:
-      parser.add_argument(
-        '--seed-file',
-          type = text_type,
-          dest = 'seed_file',
+            seed = (
+                self.seed_from_filepath(seed_filepath)
+                if seed_filepath
+                else self.prompt_for_seed()
+            )
 
-          help =
-            'Path to a file containing your seed in cleartext. '
-            'If not provided, you will be prompted to enter your seed '
-            'via stdin.',
-      )
+        arguments['api'] = Iota(
+                adapter=arguments.pop('uri'),
+                seed=seed,
+                testnet=arguments.pop('testnet'))
 
-    parser.add_argument(
-      '--testnet',
-        action  = 'store_true',
-        default = False,
-        help    = 'If set, use testnet settings (e.g., for PoW).',
-    )
+        return arguments
 
-    return parser
+    def create_argument_parser(self):
+        # type: () -> ArgumentParser
+        """
+        Returns the argument parser that will be used to interpret
+        arguments and options from argv.
+        """
+        parser = ArgumentParser(
+            description=self.__doc__,
+            epilog='PyOTA v{version}'.format(version=__version__))
 
-  @staticmethod
-  def seed_from_filepath(filepath):
-    # type: (Text) -> Seed
-    """
-    Reads a seed from the first line of a text file.
+        parser.add_argument(
+            '--uri',
+            type=text_type,
+            default='http://localhost:14265/',
+            help='URI of the node to connect to '
+                 '(defaults to http://localhost:14265/).')
 
-    Any lines after the first are ignored.
-    """
-    with open(filepath, 'rb') as f_:
-      return Seed(f_.readline().strip())
+        if self.requires_seed:
+            parser.add_argument(
+                '--seed-file',
+                type=text_type,
+                dest='seed_file',
+                help='Path to a file containing your seed in cleartext. '
+                     'If not provided, you will be prompted'
+                     ' to enter your seed via stdin.')
 
-  @staticmethod
-  def prompt_for_seed():
-    # type: () -> Seed
-    """
-    Prompts the user to enter their seed via stdin.
-    """
-    seed = secure_input(
-      'Enter seed and press return (typing will not be shown).\n'
-      'If no seed is specified, a random one will be used instead.\n'
-    )
+        parser.add_argument(
+            '--testnet',
+            action='store_true',
+            default=False,
+            help='If set, use testnet settings (e.g., for PoW).')
 
-    if isinstance(seed, text_type):
-      seed = seed.encode('ascii')
+        return parser
 
-    return Seed(seed) if seed else Seed.random()
+    @staticmethod
+    def seed_from_filepath(filepath):
+        # type: (Text) -> Seed
+        """
+        Reads a seed from the first line of a text file.
+
+        Any lines after the first are ignored.
+        """
+        with open(filepath, 'rb') as f_:
+            return Seed(f_.readline().strip())
+
+    @staticmethod
+    def prompt_for_seed():
+        # type: () -> Seed
+        """
+        Prompts the user to enter their seed via stdin.
+        """
+        seed = secure_input(
+            'Enter seed and press return (typing will not be shown).\n'
+            'If no seed is specified, a random one will be used instead.\n'
+        )
+
+        if isinstance(seed, text_type):
+            seed = seed.encode('ascii')
+
+        return Seed(seed) if seed else Seed.random()

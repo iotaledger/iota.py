e5f3f8259af7e9aba11c1ad2686005ba2d52b00a
Phoenix Zerin <phx@phx.ph>
2018-06-11 08:10:41 +1200

    Cleaned up code file, added extra docs.

--- iota/commands/extended/helpers.py
+++ iota/commands/extended/helpers.py
@@ -1,18 +1,28 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
+
+from iota.transaction.types import TransactionHash
+
+
 class Helpers(object):
-  """
-  Adds additional helper functions that aren't part of the core or extended
-  API.
-  """
-
-  def __init__(self, api):
-    self.api = api
-
-  def is_promotable(self, tail):
-    # type: (TransactionHash) -> bool
     """
-    Determines if a tail transaction is promotable.
+    Adds additional helper functions that aren't part of the core or
+    extended API.
 
-    :param tail:
-      Transaction hash. Must be a tail transaction.
+    See https://github.com/iotaledger/iota.lib.py/pull/124 for more
+    context.
     """
-    return self.api.check_consistency(tails=[tail])['state']
+
+    def __init__(self, api):
+        self.api = api
+
+    def is_promotable(self, tail):
+        # type: (TransactionHash) -> bool
+        """
+        Determines if a tail transaction is promotable.
+
+        :param tail:
+            Transaction hash. Must be a tail transaction.
+        """
+        return self.api.check_consistency(tails=[tail])['state']

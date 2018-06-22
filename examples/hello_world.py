1318039f888caeee8def746b8c24985ca1a9d9b8
Phoenix Zerin <phx@phx.ph>
2018-06-23 11:12:45 +1200

    [#145] Reformatted examples for PEP-8.

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -5,7 +5,7 @@ your friendly neighborhood node.
 """
 
 from __future__ import absolute_import, division, print_function, \
-  unicode_literals
+    unicode_literals
 
 from argparse import ArgumentParser
 from pprint import pprint
@@ -19,36 +19,39 @@ from iota import BadApiResponse, StrictIota, __version__
 
 
 def main(uri):
-  # type: (Text) -> None
-  api = StrictIota(uri)
+    # type: (Text) -> None
+    api = StrictIota(uri)
 
-  try:
-    node_info = api.get_node_info()
-  except ConnectionError as e:
-    print("Hm.  {uri} isn't responding.  Is the node running?".format(uri=uri))
-    print(e)
-  except BadApiResponse as e:
-    print("Looks like {uri} isn't very talkative today ):".format(uri=uri))
-    print(e)
-  else:
-    print('Hello {uri}!'.format(uri=uri))
-    pprint(node_info)
+    try:
+        node_info = api.get_node_info()
+    except ConnectionError as e:
+        print(
+            "Hm.  {uri} isn't responding; is the node running?".format(uri=uri)
+        )
+        print(e)
+    except BadApiResponse as e:
+        print("Looks like {uri} isn't very talkative today ):".format(uri=uri))
+        print(e)
+    else:
+        print('Hello {uri}!'.format(uri=uri))
+        pprint(node_info)
 
 
 if __name__ == '__main__':
-  parser = ArgumentParser(
-    description = __doc__,
-    epilog      = 'PyOTA v{version}'.format(version=__version__),
-  )
+    parser = ArgumentParser(
+        description=__doc__,
+        epilog='PyOTA v{version}'.format(version=__version__),
+    )
 
-  parser.add_argument(
-    '--uri',
-      type    = text_type,
-      default = 'http://localhost:14265/',
+    parser.add_argument(
+        '--uri',
+        type=text_type,
+        default='http://localhost:14265/',
 
-      help =
-        'URI of the node to connect to '
-        '(defaults to http://localhost:14265/).',
-  )
+        help=(
+            'URI of the node to connect to '
+            '(defaults to http://localhost:14265/).'
+        ),
+    )
 
-  main(**vars(parser.parse_args(argv[1:])))
+    main(**vars(parser.parse_args(argv[1:])))

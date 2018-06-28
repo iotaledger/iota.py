d1d36465508f10bb5d53cb6b0803e35b0b5389af
Phoenix Zerin <phx@phx.ph>
2018-06-28 17:00:10 +0900

    [#145] Maintain PEP-8 on `develop` branch.

--- iota/commands/core/find_transactions.py
+++ iota/commands/core/find_transactions.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, division, print_function, \
 import filters as f
 from six import iteritems
 
-from iota import Tag, TransactionHash, BundleHash
+from iota import BundleHash, Tag, TransactionHash
 from iota.commands import FilterCommand, RequestFilter, ResponseFilter
 from iota.filters import AddressNoChecksum, Trytes
 

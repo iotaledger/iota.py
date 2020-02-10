c0bbcad0d1729604556dbdc07755d860114ae05b
Levente Pap <levente.pap@iota.org>
2020-02-10 13:36:09 +0100

    Make `IsReattachableCommand` async
    - Update tests for async

--- test/commands/extended/is_reattachable_test.py
+++ test/commands/extended/is_reattachable_test.py
@@ -8,10 +8,10 @@ import filters as f
 from filters.test import BaseFilterTestCase
 from six import text_type
 
-from iota import Address, Iota
-from iota.adapter import MockAdapter
+from iota import Address, Iota, AsyncIota
+from iota.adapter import MockAdapter, async_return
 from iota.commands.extended.is_reattachable import IsReattachableCommand
-from test import patch, MagicMock
+from test import patch, MagicMock, async_test
 
 
 class IsReattachableRequestFilterTestCase(BaseFilterTestCase):
@@ -199,18 +199,41 @@ class IsReattachableCommandTestCase(TestCase):
 
   def test_wireup(self):
     """
-    Verify that the command is wired up correctly.
+    Verify that the command is wired up correctly. (sync)
 
     The API method indeed calls the appropiate command.
     """
     with patch('iota.commands.extended.is_reattachable.IsReattachableCommand.__call__',
-              MagicMock(return_value='You found me!')
+              MagicMock(return_value=async_return('You found me!'))
               ) as mocked_command:
 
       api = Iota(self.adapter)
 
       # Don't need to call with proper args here.
       response = api.is_reattachable('addresses')
+
+      self.assertTrue(mocked_command.called)
+
+      self.assertEqual(
+        response,
+        'You found me!'
+      )
+
+  @async_test
+  async def test_wireup_async(self):
+    """
+    Verify that the command is wired up correctly. (async)
+
+    The API method indeed calls the appropiate command.
+    """
+    with patch('iota.commands.extended.is_reattachable.IsReattachableCommand.__call__',
+              MagicMock(return_value=async_return('You found me!'))
+              ) as mocked_command:
+
+      api = AsyncIota(self.adapter)
+
+      # Don't need to call with proper args here.
+      response = await api.is_reattachable('addresses')
 
       self.assertTrue(mocked_command.called)
 
